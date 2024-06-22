df806eb8087f1ee41f94b0e2c466540878f43500
Gabriel Camurça <projetosdocamurca@gmail.com>
2024-06-22 17:40:05 -0300

    Implements Client Struct

--- haversine.go
+++ haversine.go
@@ -31,9 +31,14 @@ type Restaurant struct {
 	Distance  float64
 }
 
-func findClosestRestaurants(clientLat, clientLon float64, restaurants []Restaurant) []Restaurant {
+type Client struct {
+	Latitude  float64
+	Longitude float64
+}
+
+func findClosestRestaurants(client Client, restaurants []Restaurant) []Restaurant {
 	for i := range restaurants {
-		restaurants[i].Distance = haversineDistance(clientLat, clientLon, restaurants[i].Latitude, restaurants[i].Longitude)
+		restaurants[i].Distance = haversineDistance(client.Latitude, client.Longitude, restaurants[i].Latitude, restaurants[i].Longitude)
 	}
 
 	sort.Slice(restaurants, func(i, j int) bool {
@@ -44,16 +49,18 @@ func findClosestRestaurants(clientLat, clientLon float64, restaurants []Restaura
 }
 
 func main() {
-	clientLat := -23.550520
-	clientLon := -46.633308
+	client := Client{Latitude: -23.5505, Longitude: -46.6333}
 
 	restaurants := []Restaurant{
 		{Name: "Restaurante do João", Latitude: -23.5489, Longitude: -46.6388},
 		{Name: "Lanchonete da Maria", Latitude: -23.5510, Longitude: -46.6340},
 		{Name: "Cafeteria Chique", Latitude: -23.5530, Longitude: -46.6400},
+		{Name: "Pizzaria do Zé", Latitude: -23.5400, Longitude: -46.6300},
+		{Name: "Padaria Pão Quente", Latitude: -23.5450, Longitude: -46.6350},
+		{Name: "Restaurante Vegetariano", Latitude: -23.5520, Longitude: -46.6310},
 	}
 
-	closestRestaurants := findClosestRestaurants(clientLat, clientLon, restaurants)
+	closestRestaurants := findClosestRestaurants(client, restaurants)
 
 	for _, restaurant := range closestRestaurants {
 		fmt.Printf("Nome: %s, Distância %.2f km\n", restaurant.Name, restaurant.Distance)
